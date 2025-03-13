6d9154fb6b8d0be718a6ca1d20d4e0392f3b1ae9
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-03-13 20:16:27 -0300

    feat: pagina home

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,9 +1,71 @@
-export default async function Home() {
-  await new Promise((resolve) =>
-    setTimeout(() => {
-      resolve(null)
-    }, 2000),
-  )
+import Image from 'next/image'
+import Link from 'next/link'
 
-  return <div>hello world</div>
+export default function Home() {
+  return (
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href="/"
+        className=" relative group col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          width={920}
+          height={920}
+          quality={100}
+          className="group-hover:scale-105 transition-transform duration-500"
+          src="/moletom-never-stop-learning.png"
+          alt="moletom preto com o texto never stop learning"
+        />
+
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moletom AI side</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="relative group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          width={920}
+          height={920}
+          quality={100}
+          className="group-hover:scale-105 transition-transform duration-500"
+          src="/moletom-ai-side.png"
+          alt="moletom preto com o texto never stop learning"
+        />
+
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moletom AI side</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="relative group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+      >
+        <Image
+          width={920}
+          height={920}
+          quality={100}
+          className="group-hover:scale-105 transition-transform duration-500"
+          src="/camiseta-dowhile-2022.png"
+          alt="moletom preto com o texto never stop learning"
+        />
+
+        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="text-sm truncate">Moletom AI side</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+    </div>
+  )
 }
